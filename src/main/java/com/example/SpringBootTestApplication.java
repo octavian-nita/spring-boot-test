b79c9570cc21faceee6fed6b03c382cd06dc7345
mairongxin <281191779@qq.com>
2016-12-06 19:52:30 +0800

    加上监控jar包

--- src/main/java/com/example/SpringBootTestApplication.java
+++ src/main/java/com/example/SpringBootTestApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -13,6 +14,7 @@ public class SpringBootTestApplication {
     private static final Logger log = LoggerFactory.getLogger(SpringBootTestApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootTestApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootTestApplication.class, args);
+        System.out.println(context.getEnvironment().getProperty("spring.config.name"));
     }
 }
